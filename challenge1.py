6773a2ed13e6327a1e4c2135f03947c2a995a813
dmsigmon <dmsigmon@users.noreply.github.com>
2021-08-09 20:17:20 +0000

    Challenge 1 complete

--- challenge1.py
+++ challenge1.py
@@ -1,11 +1,27 @@
 !# /usr/bin/dev python3
 
+#NDE CHALLENGE #1 - Print
+
+#Set the following variables
+
+#a = "little tough to create. Especially"
+#b = "ones that are mixed up."
+#c = "Sentences are sometimes a"
+
+#Use the print function to print the following.
+
+#Sentences are sometimes a @@@
+#little tough to create. Especially @@@
+#ones that are mixed up.
+
+#Use ONLY the variables (a, b, c) as the *objects that get printed
+#Use both the sep= and end= to accomplish this task.
 
 a = "little tough to create. Especially"
 b = "ones that are mixed up."
 c = "Sentences are sometimes a"
 
-print(c, sep=" ", end=" @@@ \n")
-print(a, sep=" ", end=" @@@ \n")
-print(c, end="\n")
+
+
+print(c, a, b, sep=" @@@ \n", end="\n")
 
