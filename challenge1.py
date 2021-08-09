38a8616976bfe71bcd4b80b79d9118b635cc47d8
dmsigmon <dmsigmon@users.noreply.github.com>
2021-08-09 20:00:01 +0000

    Challenge 1 complete

---
+++ challenge1.py
@@ -0,0 +1,11 @@
+!# /usr/bin/dev python3
+
+
+a = "little tough to create. Especially"
+b = "ones that are mixed up."
+c = "Sentences are sometimes a"
+
+print(c, sep=" ", end=" @@@ \n")
+print(a, sep=" ", end=" @@@ \n")
+print(c, end="\n")
+
