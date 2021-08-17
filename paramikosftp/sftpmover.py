b5d3a50d93e0dc94bec45785f56c8f8044df6b7c
dmsigmon <dmsigmon@users.noreply.github.com>
2021-08-17 15:33:10 +0000

    sftp with un and pw

---
+++ paramikosftp/sftpmover.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import os 
+import paramiko
+
+t = paramiko.Transport("10.10.2.3", 22)
+
+t.connect(username="bender", password="alta3")
+
+sftp = paramiko.SFTPClient.from_transport(t)
+
+for x in os.listdir("/home/student/filestocopy/"):
+    if not os.path.isdir("/home/student/filestocopy/"+x):
+        sftp.put("/home/student/filestocopy/"+x, "/tmp/"+x)
+
+sftp.close()
